1cb8be04c0fbfac86b5305336873543fdfceec84
xlaez <utibeabasiekong2@gmail.com>
2023-11-28 18:23:45 +0100

    update: manually add description and version because reading package.json fails

--- lib/utils/package_description.ts
+++ lib/utils/package_description.ts
@@ -1,7 +1,12 @@
 import chalk from "chalk";
 import { program } from "commander";
-import { description, version } from "./read_package_json.js";
 
 export const packageDescription = () => {
-  program.version(version).description(chalk.cyan(description));
+  program
+    .version("1.0.3")
+    .description(
+      chalk.cyan(
+        "The dolphjs-cli is a tool for efficient intialization and management of a dolphjs project"
+      )
+    );
 };
