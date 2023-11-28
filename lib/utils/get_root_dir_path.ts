be3a0a22c5fc4e1ec19efe0305f80b93f6f1b6a6
xlaez <utibeabasiekong2@gmail.com>
2023-11-28 18:10:08 +0100

    update: fix error with file paths

--- lib/utils/get_root_dir_path.ts
+++ lib/utils/get_root_dir_path.ts
@@ -1,12 +1,7 @@
 import path from "path";
-import { config } from "dotenv";
-config();
 
 export const getRootDirectory = () => {
   let projectRoot: string;
-  projectRoot =
-    process.env?.mode === "dev"
-      ? path.join(process.cwd(), "src")
-      : path.join(process.cwd());
+  projectRoot = path.join(process.cwd());
   return projectRoot;
 };
