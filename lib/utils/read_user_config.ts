02bfb855c21f80699b7aa20446cc053048c12711
xlaez <utibeabasiekong2@gmail.com>
2024-01-05 12:18:41 +0100

    update: config command and options modified

--- lib/utils/read_user_config.ts
+++ lib/utils/read_user_config.ts
@@ -11,122 +11,120 @@ export const configurePackage = () => {
     .alias("cf")
     .description("Set default configurations")
     .option(
-      "-l, --language" + chalk.bold(chalk.blue("<language>")),
+      "-lg, --language" + chalk.bold(chalk.blue("<language>")),
       "Set the default coding language (ts or js)"
     )
     .option(
-      "-p, --paradigm" + chalk.bold(chalk.blue("<paradigm>")),
+      "-pd, --paradigm" + chalk.bold(chalk.blue("<paradigm>")),
       "Set the default programming paradigm (oop or functional)"
     )
     .option(
-      "-d, --database" + chalk.bold(chalk.blue("<database>")),
+      "-db, --database" + chalk.bold(chalk.blue("<database>")),
       "Set the default project database (mongo, mysql, postresql or other)"
     )
     .option(
       "-gf, --generateFolder" + chalk.bold(chalk.blue("<boolean>")),
       "Generates a folder file for the Services, Models, Controllers and Routes (true or false)"
     )
-    .action((options) => {
+    .action(async (name: any, options: any) => {
       const config = readConfig();
 
-      // validate language choosen
-
-      if (options.language) {
-        if (options.language !== "js" && options.language !== "ts") {
-          console.log(options.language);
-
+      Object.entries(name).forEach(([key, value]) => {
+        if (key && key.toLocaleLowerCase().includes("language") && value) {
+          if (value.toString() !== "js" && value.toString() !== "ts") {
+            console.log(
+              chalk.red(
+                `Invalid project language, format must be either ${chalk.bold(
+                  "js"
+                )} or ${chalk.bold("ts")}`
+              )
+            );
+            return;
+          }
+          config.language = options.language;
+          writeConfig(config);
           console.log(
-            chalk.red(
-              `Invalid project language, format must be either ${chalk.bold(
-                "js"
-              )} or ${chalk.bold("ts")}`
-            )
-          );
-          return;
-        }
-        config.language = options.language;
-        writeConfig(config);
-        console.log(
-          chalk.cyan(
-            `Default project language is now ${chalk.bold(config.language)}`
-          )
-        );
-        return;
-      }
-
-      // validate paradigm choosen
-      if (options.paradigm) {
-        if (options.paradigm !== "oop" && options.paradigm !== "functional") {
-          console.log(
-            chalk.red(
-              `Invalid programming paradigm, must be either ${chalk.bold(
-                "oop"
-              )} or ${chalk.bold("functional")}`
+            chalk.cyan(
+              `Default project language is now ${chalk.bold(config.language)}`
             )
           );
           return;
         }
 
-        config.paradigm = options.paradigm;
-        writeConfig(config);
-        console.log(
-          chalk.cyan(
-            `Default programming paradigm is now ${chalk.bold(config.paradigm)}`
-          )
-        );
-        return;
-      }
+        if (key && key.toLocaleLowerCase().includes("paradigm") && value) {
+          if (value.toString() !== "oop" && value.toString() !== "functional") {
+            console.log(
+              chalk.red(
+                `Invalid programming paradigm, must be either ${chalk.bold(
+                  "oop"
+                )} or ${chalk.bold("functional")}`
+              )
+            );
+            return;
+          }
 
-      // validate database choosen
-      if (options.database) {
-        if (!dbOptions.includes(options.database)) {
+          config.paradigm = options.paradigm;
+          writeConfig(config);
           console.log(
-            chalk.red(
-              `Invalid database value, must be either of these: ${dbOptions.join(
-                ", "
-              )} `
+            chalk.cyan(
+              `Default programming paradigm is now ${chalk.bold(
+                config.paradigm
+              )}`
             )
           );
           return;
         }
 
-        config.database = options.database;
-        writeConfig(config);
-        console.log(
-          chalk.cyan(
-            `Default project database is now ${chalk.bold(config.database)}`
-          )
-        );
-        return;
-      }
+        if (key && key.toLocaleLowerCase().includes("database") && value) {
+          if (!dbOptions.includes(value.toString())) {
+            console.log(
+              chalk.red(
+                `Invalid database value, must be either of these: ${dbOptions.join(
+                  ", "
+                )} `
+              )
+            );
+            return;
+          }
+
+          config.database = options.database;
+          writeConfig(config);
+          console.log(
+            chalk.cyan(
+              `Default project database is now ${chalk.bold(config.database)}`
+            )
+          );
+          return;
+        }
+      });
 
       // validate generateFolder
 
-      if (options.generateFolder) {
-        if (
-          options.generateFolder !== "true" &&
-          options.generateFolder !== "false"
-        ) {
-          console.log(
-            chalk.red(
-              `Invalid folder generation value, value must be ${chalk.bold(
-                "true"
-              )} or ${chalk.bold("false")}`
-            )
-          );
-          return;
-        }
-        config.generateFolder = options.generateFolder;
-        writeConfig(config);
-        console.log(
-          chalk.cyan(
-            `Generation of folder files for the Services, Models, Controllers and Routes  set to ${chalk.bold(
-              config.generateFolder
-            )}`
-          )
-        );
-        return;
-      }
+      // if (options.generateFolder) {
+      //   if (
+      //     options.generateFolder !== "true" &&
+      //     options.generateFolder !== "false"
+      //   ) {
+      //     console.log(
+      //       chalk.red(
+      //         `Invalid folder generation value, value must be ${chalk.bold(
+      //           "true"
+      //         )} or ${chalk.bold("false")}`
+      //       )
+      //     );
+      //     return;
+      //   }
+      //   config.generateFolder = options.generateFolder;
+      //   writeConfig(config);
+      //   console.log(
+      //     chalk.cyan(
+      //       `Generation of folder files for the Services, Models, Controllers and Routes  set to ${chalk.bold(
+      //         config.generateFolder
+      //       )}`
+      //     )
+      //   );
+      //   return;
+      // }
 
       if (Object.keys(options).length === 0) {
         console.log(
@@ -138,6 +136,7 @@ export const configurePackage = () => {
             )
           )
         );
+        process.exit(0);
       }
     });
 };
