d05c3c68008b924b9d711039c56b6419d242d00d
Utee <utibeabasiekong2@gmail.com>
2024-07-19 02:29:54 +0100

    update: templates and version of dolphjs

--- lib/utils/package_description.ts
+++ lib/utils/package_description.ts
@@ -3,7 +3,7 @@ import { program } from "commander";
 
 export const packageDescription = () => {
   program
-    .version("1.2.0")
+    .version("1.2.1")
     .description(
       chalk.cyan(
         "The dolph-cli is a tool for efficient intialization and management of a dolphjs project"
